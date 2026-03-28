410a10866c17a217fb59d45844ff131817fa4736
2k25csaiml2513533-gif <2k25csaiml2513533@gmail.com>
2026-03-29 00:39:02 +0530

    variables,constant and datatypes

---
+++ modules/01_variables.js
@@ -0,0 +1,7 @@
+const accid=112233;
+let accmail="rohanverma@gmail.com";
+var accpassword="jaipur";
+//accid=2     "NOT ALLOWED";
+// prefer not to use var because of 
+// issue in block scope and functional scope
+console.log(accid);
---
+++ modules/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+let name= "rohan"
+let num=3
+let option=true
+// number=> 2 to power 53
+// BigInt
+// String=""
+// boolean=>true/false
+// null =>standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+console.log(typeof undefined);
+console.log(typeof null);
