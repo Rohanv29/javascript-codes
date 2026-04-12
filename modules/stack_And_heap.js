791a4f0863d12f08827941198aa77b66103ea465
2k25csaiml2513533-gif <2k25csaiml2513533@gmail.com>
2026-04-12 11:17:50 +0530

    memory_basics

---
+++ modules/stack_And_heap.js
@@ -0,0 +1,17 @@
+const { use } = require("react");
+
+//_________STACK MEMORY____________________
+let abcid="rohan786"
+let verma=abcid
+verma="Arpit"
+console.log(abcid);
+console.log(verma);
+//________HEAP MEMORY________________________
+let user1={
+    email:"user@gmail.com",
+    upiId:"user@ybl"
+}
+let user2=user1
+user2.email="rohan@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
